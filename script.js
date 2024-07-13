219eb58ce59a6c00cae9d1a27afa4419d1f2def1
Ray <170437057+raywang38@users.noreply.github.com>
2024-07-14 00:11:41 +1000

    add 'Back' function, refactor '.' code logic
    Added a 'Back' button to trim off last digit in
    display field, refactored how the '.' is added to
    the current displayed number, also refactored how
    "+/-" is interpreted so that it works properly.

--- script.js
+++ script.js
@@ -7,15 +7,19 @@ const displayField = document.querySelector(".displayField");
 const numButtons = document.querySelectorAll(".numpad button")
 
 numButtons.forEach((numButton) => {
-    numButton.addEventListener("click", () => {            
-        if (!operator) {
-            num1 = parseInt(numButton.textContent);
-            displayField.textContent = num1;
+    numButton.addEventListener("click", () => {
+        if (numButton.textContent === "." && !(displayField.textContent.includes("."))) {
+            displayField.textContent += numButton.textContent;
+            return;
         }
-        else {
-            num2 = parseInt(numButton.textContent);
-            displayField.textContent = num2;
+        else if (numButton.textContent === "." && displayField.textContent.includes(".")) {
+            return;
         }
+        else if (numButton.textContent === "Back") {
+            displayField.textContent = displayField.textContent.slice(0, -1);
+            return;
+        }
+        displayField.textContent += numButton.textContent;          
     })
 })
 
@@ -24,12 +28,16 @@ const operatorButtons = document.querySelectorAll(".operators button")
 operatorButtons.forEach((operatorButton) => {
     operatorButton.addEventListener("click", () => {
         if (operatorButton.textContent === "=") {
-            const total = operate(num1, num2, operator);
+            num2 = parseFloat(displayField.textContent)
+            const total = operate(num1, num2, operator).toFixed(2) * 1;
             displayField.textContent = total;
             num1 = total;
+            operator = "";
         }
         else {
+            num1 = parseFloat(displayField.textContent);
             operator = operatorButton.textContent;
+            displayField.textContent = "";
         }
     })
 })
@@ -38,11 +46,18 @@ const topPadButtons = document.querySelectorAll(".toppad button")
 
 topPadButtons.forEach((topPadButton) => {
     topPadButton.addEventListener("click", () => {
-        if (topPadButton.textContent === "%") {
-            operator = topPadButton.textContent;
+        if (topPadButton.textContent === "+/-") {
+            if (displayField.textContent.includes("-")) {
+                displayField.textContent = displayField.textContent.replace("-", "");
+            }
+            else {
+                displayField.textContent = "-" + displayField.textContent;
+            }
         }
-        else if (topPadButton.textContent === "+/-") {
-
+        else if (topPadButton.textContent === "%") {
+            num1 = parseFloat(displayField.textContent);
+            operator = topPadButton.textContent;
+            displayField.textContent = "";
         }
         else if (topPadButton.textContent === "AC") {
             num1 = "";
